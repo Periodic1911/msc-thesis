f20b88be35cd7ce86f9ede096e315cda444b576d
Anne Stijns <anstijns@gmail.com>
2024-10-29 16:14:04 +0100

    make stage_f mem use sysMEM blocks

--- combi/stage_f/mem.sv
+++ combi/stage_f/mem.sv
@@ -1,9 +1,12 @@
 /* Read-Only Memory block */
-module prog_mem #(parameter SIZE_LOG2=13) (input clk, input rst, input [SIZE_LOG2-1:0] A, output [31:0] RD);
+module prog_mem #(parameter SIZE_LOG2=13) (input clk, input [SIZE_LOG2-1:0] A, output [31:0] RD);
   logic [31:0] mem_ff [2**SIZE_LOG2-1:0];
   logic [31:0] read;
 
-  assign RD = mem_ff[A];
+  assign RD = read;
+
+  always_ff @(posedge clk)
+    read = mem_ff[A];
 
   /* initialize memory */
   initial begin

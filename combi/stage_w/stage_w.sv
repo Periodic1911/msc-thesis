87a8736d34268a9ab9b0ce19c41709459c74c88e
Anne Stijns <anstijns@gmail.com>
2024-11-12 17:36:11 +0100

    ifdef W stage

--- combi/stage_w/stage_w.sv
+++ combi/stage_w/stage_w.sv
@@ -2,43 +2,86 @@ module stage_w (
   input logic clk, rst,
 
   input logic [31:0] ALUResultM, ReadDataW,
+`ifdef RISCV
   input logic [31:0] PCPlus4M, // RV only
+`endif
   input logic [4:0] RdM,
+`ifdef RISCV `ifdef ARM
   input logic armM, // combi only
+`endif `endif
 
+`ifdef ARM
   input logic PCSrcM, // ARM only
+`endif
   input logic RegWriteM,
+`ifdef RISCV
   input logic [1:0] ResultSrcM, // bit 1 RV only
+`else
+  input logic ResultSrcM, // bit 1 RV only
+`endif
 
   output logic [4:0] RdW,
   output logic [31:0] ResultW,
+`ifdef ARM
   output logic PCSrcW, // ARM only
-  output logic RegWriteW,
-  output logic armW // combi only
+`endif
+`ifdef RISCV `ifdef ARM
+  output logic armW, // combi only
+`endif `endif
+  output logic RegWriteW
   );
 
 logic [1:0] ResultSrcW;
+`ifdef RISCV
 logic [31:0] PCPlus4W; // RV only
+`endif
 logic [31:0] ALUResultW;
 
-flopr #(74) em_stage(clk, rst,
+`ifdef RISCV
+flopr #(33) em_stage_riscv(clk, rst,
+  {
+   PCPlus4M, // RV only
+   ResultSrcM[1] // bit 1 RV only
+  },
+  {
+   PCPlus4W, // RV only
+   ResultSrcW[1] // bit 1 RV only
+  }
+);
+`endif
+
+`ifdef ARM
+flopr #(1) em_stage_arm(clk, rst,
+   PCSrcM, // ARM only
+   PCSrcW // ARM only
+);
+`endif
+
+`ifdef RISCV `ifdef ARM
+flopr #(1) em_stage_combi(clk, rst,
+   armM,
+   armW
+);
+`endif `endif
+
+flopr #(39) em_stage(clk, rst,
   {
    ALUResultM,
-   PCPlus4M, // RV only
    RdM,
-   PCSrcM, // ARM only
    RegWriteM,
-   armM,
-   ResultSrcM // bit 1 RV only
+   ResultSrcM
+   `ifdef RISCV
+     [0] // bit 1 RV only
+   `endif
   },
   {
    ALUResultW,
-   PCPlus4W, // RV only
    RdW,
-   PCSrcW, // ARM only
    RegWriteW,
-   armW,
-   ResultSrcW // bit 1 RV only
+   ResultSrcW
+   `ifdef RISCV
+     [0] // bit 1 RV only
+   `endif
   }
   );
 

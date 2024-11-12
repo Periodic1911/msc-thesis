c147e956d1ffa15be8ae2b27c8176f61747fe3a5
Anne Stijns <anstijns@gmail.com>
2024-11-12 17:25:24 +0100

    ifdef stage e

--- combi/stage_e/alu.sv
+++ combi/stage_e/alu.sv
@@ -3,14 +3,22 @@ module alu(
   input logic [31:0] Op1E, Op2E,
 
   output logic [31:0] ALUResultE,
+`ifdef ARM
   output logic [3:0] ALUFlags, // ARM only
+`endif
+`ifdef RISCV
   output logic ZeroE // RISC-V only
+`endif
 );
 
 logic [31:0] addResult;
 
-add_sub as(.a(Op1E), .b(Op2E), .q(addResult), .add(~ALUControlE[0]), .cOut(carry),
-  .overflow(overflow) // ARM only
+add_sub as(.a(Op1E), .b(Op2E), .q(addResult), .add(~ALUControlE[0])
+`ifdef ARM
+  ,
+  .overflow(overflow), // ARM only
+  .cOut(carry)
+`endif
   );
 
 always_comb
@@ -19,13 +27,19 @@ always_comb
     3'b001: ALUResultE = addResult; // sub
     3'b010: ALUResultE = Op1E & Op2E; // and
     3'b011: ALUResultE = Op1E | Op2E; // or
+`ifdef RISCV
     // RISC-V only
-    3'b101: ALUResultE = (Op1E < Op2E) ? 32'b1 : 32'b0; // slt TODO: reuse add_sub carry chain?
+    3'b101: ALUResultE = (Op1E < Op2E) ? 32'b1 : 32'b0; // slt
+    //TODO: reuse add_sub carry chain?
+`endif
     default: ALUResultE = 32'hxxxxxxxx; /// ???
   endcase
 
+`ifdef RISCV
 assign ZeroE = (ALUResultE == 0); // RISC-V only
+`endif
 
+`ifdef ARM
 logic neg, zero, carry, overflow; // ARM only
 assign ALUFlags = {neg, zero, carry, overflow}; // ARM only
 
@@ -33,6 +47,7 @@ always_comb begin : ARM_Flags // ARM only
   zero = (ALUResultE == 0);
   neg = ALUResultE[31];
 end
+`endif
 
 endmodule
 
@@ -41,18 +56,26 @@ module add_sub(
   input logic [31:0] a, b,
   input logic add,
   output logic [31:0] q,
+`ifdef ARM
   output logic cOut,
   output logic overflow // ARM only
+`endif
 );
 
 logic [31:0] b_inv;
 assign b_inv = add ? b : ~b;
 logic carry_in = ~add;
-logic carry_out;
 
+`ifdef ARM
+logic carry_out;
 assign {carry_out, q} = a + b_inv + {31'b0, carry_in};
+
 xor(cOut, carry_out, carry_in);
 assign overflow = (~q[31] & a[31] & b_inv[31]) |
                   ( q[31] &~a[31] &~b_inv[31]);
+
+`else
+assign q = a + b_inv + {31'b0, carry_in};
+`endif
 
 endmodule
--- combi/stage_e/stage_e.sv
+++ combi/stage_e/stage_e.sv
@@ -4,31 +4,57 @@ module stage_e(
   input logic [31:0] Rd1D, Rd2D,
   input logic [31:0] immextD,
   input logic [4:0] RdD,
+`ifdef RISCV
   input logic [31:0] PCD, PCPlus4D, // RISC-V only
   input logic [4:0] Rs1D, Rs2D, // RISC-V only
+`ifdef ARM
   input logic armD, // combi only
+`endif
+`endif
 
+`ifdef RISCV
   output logic [31:0] PCPlus4E, // RISC-V only
+`endif
   output logic [4:0] RdE,
   output logic [31:0] WriteDataE, ALUResultE,
 
   /* control inputs */
   input logic RegWriteD, MemWriteD, BranchD, ALUSrcD,
   input logic [2:0] ALUControlD,
+`ifdef ARM
   input logic PCSrcD, // ARM only
   input logic [1:0] FlagWriteD, // ARM only
   input logic [3:0] CondD, // ARM only
-  input logic [1:0] ResultSrcD, // RISC-V only
+`endif
+`ifdef RISCV
+  input logic [1:0] ResultSrcD, // bit 1 is RISC-V only
+`else
+  input logic ResultSrcD,
+`endif
+`ifdef RISCV
   input logic JumpD, // RISC-V only
+`endif
 
+`ifdef RISCV
   output logic [1:0] ResultSrcE, // bit 1 is RISC-V only
+`else
+  output logic ResultSrcE,
+`endif
+`ifdef ARM
   output logic PCSrcE, // ARM only
+`endif
   output logic RegWriteE, MemWriteE,
 
+`ifdef ARM
   output logic BranchTakenE, // ARM only
+`endif
+`ifdef RISCV
   output logic RVPCSrcE, // RISC-V only
   output logic [31:0] PCTargetE, // RISC-V only
+`endif
+`ifdef ARM
   output logic armE, // combi only
+`endif
 
   input logic [31:0] ALUResultM, ResultW,
 
@@ -40,26 +66,37 @@ module stage_e(
 
 logic [31:0] Rd1E, Rd2E;
 logic [31:0] immextE;
+`ifdef RISCV
 logic [31:0] PCE; // RISC-V only
+`endif
 logic RegWrite, MemWrite, BranchE, ALUSrcE;
 logic [2:0] ALUControlE;
+`ifdef ARM
 logic PCSrc; // ARM only
 logic [1:0] FlagWriteE; // ARM only
 logic [3:0] CondE; // ARM only
+`endif
+`ifdef RISCV
 logic JumpE; // RISC-V only
+`endif
 
+`ifdef ARM
 logic [3:0] FlagsE, FlagsD; // ARM only
 
 // ARM only
 logic RegWriteE_ARM, MemWriteE_ARM;
+logic [3:0] ALUFlags; // ARM only
 assign RegWriteE = armE ? RegWriteE_ARM : RegWrite;
 assign MemWriteE = armE ? MemWriteE_ARM : MemWrite;
 condlogic condl(.*);
+`endif
 
+`ifdef RISCV
 rvbranch branch_rv(.*); // RV only
 
 logic ZeroE; // RV only
-logic [3:0] ALUFlags; // ARM only
+`endif
+
 alu myalu(.*);
 
 logic [31:0] Op1E, Op2E;
@@ -70,36 +107,61 @@ mux2 #(32)immMux2(WriteDataE, immextE, ALUSrcE, Op2E);
 
 assign PCTargetE = PCE + immextE;
 
-flopr #(197) de_stage(clk, (rst | FlushE),
+flopr #(76) de_stage_riscv(clk, (rst | FlushE),
   {
-  Rd1D, Rd2D, RdD, immextD,
   PCD, PCPlus4D, // RISC-V only
   Rs1D, Rs2D, // RISC-V only
-  /* control inputs */
-  RegWriteD, MemWriteD, BranchD, ALUSrcD,
-  ALUControlD,
+  JumpD, // RISC-V only
+  ResultSrcD[1] // bit 1 RISC-V only
+  },
+  {
+  PCE, PCPlus4E, // RISC-V only
+  Rs1E, Rs2E, // RISC-V only
+  JumpE, // RISC-V only
+  ResultSrcE[1] // bit 1 RISC-V only
+  }
+);
+
+flopr #(11) de_stage_arm(clk, (rst | FlushE),
+  {
   PCSrcD, // ARM only
   FlagWriteD, // ARM only
   CondD, // ARM only
-  FlagsD, // ARM only
-  ResultSrcD, // bit 1 RISC-V only
-  JumpD, // RISC-V only
-  armD // combi only
+  FlagsD // ARM only
   },
   {
-  Rd1E, Rd2E, RdE, immextE,
-  PCE, PCPlus4E, // RISC-V only
-  Rs1E, Rs2E, // RISC-V only
-  /* control inputs */
-  RegWrite, MemWrite, BranchE, ALUSrcE,
-  ALUControlE,
   PCSrc, // ARM only
   FlagWriteE, // ARM only
   CondE, // ARM only
-  FlagsE, // ARM only
-  ResultSrcE, // bit 1 RISC-V only
-  JumpE, // RISC-V only
-  armE // combi only
+  FlagsE // ARM only
+  }
+);
+
+flopr #(1) de_stage_combi(clk, (rst | FlushE),
+  armD, // combi only
+  armE  // combi only
+);
+
+flopr #(109) de_stage(clk, (rst | FlushE),
+  {
+  Rd1D, Rd2D, RdD, immextD,
+  /* control inputs */
+  RegWriteD, MemWriteD, BranchD, ALUSrcD,
+  ALUControlD,
+  ResultSrcD
+  `ifdef RISCV 
+    [0]
+  `endif // bit 1 RISC-V only
+  },
+  {
+  Rd1E, Rd2E, RdE, immextE,
+  /* control inputs */
+  RegWrite, MemWrite, BranchE, ALUSrcE,
+  ALUControlE,
+  ResultSrcE
+  `ifdef RISCV 
+    [0]
+  `endif // bit 1 RISC-V only
   }
 );
 

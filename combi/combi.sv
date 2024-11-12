3a941f8ad2e972780eb67b12ecd79b1af301cc87
Anne Stijns <anstijns@gmail.com>
2024-11-12 14:56:45 +0100

    parametrize top level

--- combi/combi.sv
+++ combi/combi.sv
@@ -1,65 +1,79 @@
+`define ARM
+`define RISCV
+
 module combi (
   input logic clk, rst,
   output logic [31:0] WriteData, DataAddr,
   output logic MemWrite
 );
 
-/* fetch */
-logic [31:0] RDD;
+             /* fetch */
+             logic [31:0] RDD;
 
-logic [31:0] PCF;
-logic [31:0] PCPlus4F;
+             logic [31:0] PCF;
+             logic [31:0] PCPlus4F;
 
-/* decode */
-logic [31:0] Rd1D, Rd2D;
-logic [31:0] immextD;
-logic [4:0] RdD;
-logic [31:0] PCD, PCPlus4D; // RISC-V only
-logic [4:0] Rs1D, Rs2D; // RISC-V only
-logic armD;
+             /* decode */
+             logic [31:0] Rd1D, Rd2D;
+             logic [31:0] immextD;
+             logic [4:0] RdD;
+`ifdef RISCV logic [31:0] PCD, PCPlus4D; `endif
+`ifdef RISCV logic [4:0] Rs1D, Rs2D; `endif
 
-/* control outputs */
-logic RegWriteD, MemWriteD, BranchD, ALUSrcD;
-logic [2:0] ALUControlD;
-logic PCSrcD; // ARM only
-logic [1:0] FlagWriteD; // ARM only
-logic [3:0] CondD; // ARM only
-logic [1:0] ResultSrcD; // bit 1 RISC-V only
-logic JumpD; // RISC-V only
+`ifdef RISCV
+  `ifdef ARM logic armD; `endif
+`endif
 
-/* execute */
-logic [31:0] PCPlus4E; // RISC-V only
-logic [4:0] RdE;
-logic [31:0] WriteDataE; //, ALUResultE;
+             /* control outputs */
+             logic RegWriteD, MemWriteD, BranchD, ALUSrcD;
+             logic [2:0] ALUControlD;
+`ifdef ARM   logic PCSrcD; `endif
+`ifdef ARM   logic [1:0] FlagWriteD; `endif
+`ifdef ARM   logic [3:0] CondD; `endif
+`ifdef RISCV logic JumpD; `endif
+`ifdef RISCV logic [1:0] ResultSrcD; // bit 1 RISC-V only
+`else        logic ResultSrcD; `endif
 
-logic [1:0] ResultSrcE; // bit 1 is RISC-V only
-logic PCSrcE; // ARM only
-logic RegWriteE, MemWriteE;
+             /* execute */
+             logic [4:0] RdE;
+             logic [31:0] WriteDataE;
 
-logic BranchTakenE; // ARM only
-logic RVPCSrcE; // RISC-V only
-logic [31:0] PCTargetE; // RISC-V only
-logic [31:0] ALUResultE;
+             logic RegWriteE, MemWriteE;
+             logic [31:0] ALUResultE;
+             logic [4:0] Rs1E, Rs2E;
+`ifdef ARM   logic PCSrcE; `endif
+`ifdef ARM   logic BranchTakenE; `endif
+`ifdef RISCV logic RVPCSrcE; `endif
+`ifdef RISCV logic [31:0] PCTargetE; `endif
+`ifdef RISCV logic [31:0] PCPlus4E; `endif
+`ifdef RISCV logic [1:0] ResultSrcE; // bit 1 RISC-V only
+`else        logic ResultSrcE; `endif
 
-logic [4:0] Rs1E, Rs2E;
-logic armE;
+`ifdef RISCV
+  `ifdef ARM logic armE; `endif
+`endif
 
-/* memory */
-logic [31:0] ALUResultM, ReadDataW;
-logic [31:0] PCPlus4M; // RV only
-logic [4:0] RdM;
+             /* memory */
+             logic [31:0] ALUResultM, ReadDataW;
+             logic [4:0] RdM;
+             logic RegWriteM;
+`ifdef ARM   logic PCSrcM; `endif
+`ifdef RISCV logic [31:0] PCPlus4M; `endif
+`ifdef RISCV logic [1:0] ResultSrcM; // bit 1 RV only
+`else        logic [1:0] ResultSrcM; `endif
 
-logic PCSrcM; // ARM only
-logic RegWriteM;
-logic [1:0] ResultSrcM; // bit 1 RV only
-logic armM;
+`ifdef RISCV
+  `ifdef ARM logic armM; `endif
+`endif
 
-/* writeback */
-logic [4:0] RdW;
-logic [31:0] ResultW;
-logic PCSrcW; // ARM only
-logic RegWriteW;
-logic armW;
+             /* writeback */
+             logic [4:0] RdW;
+             logic [31:0] ResultW;
+             logic RegWriteW;
+`ifdef ARM   logic PCSrcW; `endif
+`ifdef RISCV
+  `ifdef ARM logic armW; `endif
+`endif
 
 /* hazzard unit */
 logic StallF, StallD, FlushD, FlushE;
